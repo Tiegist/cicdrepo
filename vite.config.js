d3657772eb41fe8010f43f321cde85dba9be247f
Tiegist Girma <tigistgirma40@gmail.com>
2025-02-08 16:21:22 +0300

    updating package.json file to solve the problem with git action

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { createRequire } from 'module';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +14,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      crypto: require.resolve('crypto-browserify'), // Add this line
     },
   },
-})
+});
